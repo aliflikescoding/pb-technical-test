012d98f1a1277fd5dee45424ab06127c16fd1b84
Alif Wibowo <alif.likes.coding@gmail.com>
2024-12-06 21:58:42 +0700

    Make input form parent

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,49 @@
-import { useState } from 'react';
-import './App.css';
-import Input from './components/Input';
-import { useSelector, useDispatch } from 'react-redux';
+import "./App.css";
+import Input from "./components/Input";
+import { useSelector, useDispatch } from "react-redux";
 import store from "./redux/store";
-import { updateField } from './redux/slices/slices'; // adjust path as needed
+import { updateField } from "./redux/slices/slices"; // adjust path as needed
 
 function App() {
   const dispatch = useDispatch();
-  const usernameState = useSelector(state => state.form.username);
+  const usernameState = useSelector((state) => state.form.username);
 
   const handleInputChange = (e, error) => {
     const { value } = e.target;
-    
-    dispatch(updateField({
-      field: 'username',
-      value,
-      error
-    }));
-  };
 
+    dispatch(
+      updateField({
+        field: "username",
+        value,
+        error,
+      })
+    );
+  };
 
   // Validation function to be passed to Input
   const validateUsername = (value) => {
-    if (value.includes('@')) {
-      return 'Username cannot contain @';
+    if (value.includes("@")) {
+      return "Username cannot contain @";
     }
-    return '';
+    return "";
   };
 
   return (
     <>
-      <Input
-        label="Username"
-        name="username"
-        value={usernameState.value}
-        onChange={handleInputChange}
-        validate={validateUsername}
-        required
-        placeholder='John Doe'
-        minLength={3}
-        maxLength={20}
-        errorMessage={usernameState.error}
-      />
+      <div className="input-fields">
+        <Input
+          label="Username"
+          name="username"
+          value={usernameState.value}
+          onChange={handleInputChange}
+          validate={validateUsername}
+          required
+          placeholder="John Doe"
+          minLength={3}
+          maxLength={20}
+          errorMessage={usernameState.error}
+        />
+      </div>
     </>
   );
 }
